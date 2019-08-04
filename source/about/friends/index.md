07588cd0475daffbfc6e0da84353c71befd2ab22
EndureBlaze <nihaocun@163.com>
2019-08-04 20:00:27 +0800

    22

--- source/about/friends/index.md
+++ source/about/friends/index.md
@@ -6,9 +6,10 @@ comments: false
 
 * [灰灰のBLOG](https://blog.xhhold.com/)
 * [逐梦影音](http://video.zmkj6.top)
-* [Tujian](https://dpx.chimon.me/)
+* [Tujian](https://dailypics.cn/)
 * [Createlite的博客](https://www.acandroid.top/)
 * [九伏工作室的任饭聚集地](http://9-volt.ys168.com/)
 * [何言的杂货店](http://heyanle.cn/)
 * [天真的小窝](https://bin.zmide.com/)
 * [ImpulseMi 官改发布站 - For ImpulseMi Dev](https://wardens.xyz/)
+* [web1n](https://https.vc/)
--- source/js/kirbyimage.js
+++ source/js/kirbyimage.js
@@ -1,11 +1,11 @@
-photo = {
+kirbyimage = {
     page: 1,
     //offset 用于设置照片数量的上限
     offset: 100,
     init: function() {
         var that = this;
         //这里设置的是刚才生成的 json 文件路径
-        $.getJSON("/about/photos/photoslist.json", function(data) {
+        $.getJSON("/about/kirbyimage/kirbyimagelist.json", function(data) {
             that.render(that.page, data);
             //that.scroll(data);
         });
@@ -23,13 +23,13 @@ photo = {
             imageY = imageSize.split('.')[1];
             //这里 250 指的是图片的宽度，可以根据自己的需要调整相册中照片的大小
             li += '<div class="card" style="width:250px">' +
-                '<div class="ImageInCard" style="height:' + 250 * imageY / imageX + 'px">' +
+                '<div class="ImageInCard" style="height:' + 210 * imageY / imageX + 'px">' +
                 //href 和 src 的链接地址是相册照片外部链接，也可以放博客目录里
-                '<a data-fancybox="gallery" href="/about/photos/images/' + imgNameWithPattern + '?raw=true" data-caption="' + imgName + '">' +
-                '<img srcset="/about/photos/images/' + imgNameWithPattern + '?raw=true" src="/about/photos/' + imgNameWithPattern + '?raw=true"/>' +
+                '<a data-fancybox="gallery" href="/about/kirbyimage/images/' + imgNameWithPattern + '?raw=true" data-caption="' + imgName + '">' +
+                '<img srcset="/about/kirbyimage/images/' + imgNameWithPattern + '?raw=true" src="/about/kirbyimage/' + imgNameWithPattern + '?raw=true"/>' +
                 '</a>' +
                 '</div>' +
-                //'<div class="TextInCard">' + imgName + '</div>' + //图片下显示文件名作为说明的功能
+                '<div class="TextInCard">' + imgName + '</div>' + //图片下显示文件名作为说明的功能
                 '</div>'
         }
         $(".ImageGrid").append(li);
@@ -48,4 +48,4 @@ photo = {
         });
     }
 }
-photo.init();
+kirbyimage.init();
